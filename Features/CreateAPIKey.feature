f148f26eb6223c4a105a2ad8dbda64f82a25940e
gcrumpton <gcrumpton@spartaglobal.com>
2025-04-10 13:19:30 +0100

    feat: add 'CreateAPIKey.feature' and placeholder step defs

---
+++ Features/CreateAPIKey.feature
@@ -0,0 +1,9 @@
+﻿Feature: CreateAPIKey
+
+As a user, I want to be able to send a POST request containing a username and password,
+so that I get a API key as a response.
+
+Scenario: Create API key
+	Given I create a POST request with a given username and password
+	When I send the POST request
+	Then I receive a response with a 200 OK status code
---
+++ Steps/CreateAPIKeyStepDefinitions.cs
@@ -0,0 +1,29 @@
+using System;
+using Reqnroll;
+
+namespace VideoGameDatabaseTest.Steps
+{
+    [Binding]
+    public class CreateAPIKeyStepDefinitions
+    {
+        //TODO: Add logic to all step defs
+
+        [Given("I create a POST request with a given username and password")]
+        public void GivenICreateAPOSTRequestWithAGivenUsernameAndPassword()
+        {
+            throw new PendingStepException();
+        }
+
+        [When("I send the POST request")]
+        public void WhenISendThePOSTRequest()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then("I receive a response with a {int} OK status code")]
+        public void ThenIReceiveAResponseWithAOKStatusCode(int p0)
+        {
+            throw new PendingStepException();
+        }
+    }
+}
