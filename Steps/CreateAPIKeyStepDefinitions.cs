e498a05a3935448dba844ada32319569517173da
gcrumpton <gcrumpton@spartaglobal.com>
2025-04-11 14:26:40 +0100

    feat: add logic to 'CreateAPIKey.feature' step defs

--- Steps/CreateAPIKeyStepDefinitions.cs
+++ Steps/CreateAPIKeyStepDefinitions.cs
@@ -1,29 +1,45 @@
 using System;
+using NUnit.Framework;
 using Reqnroll;
+using RestSharp;
 
 namespace VideoGameDatabaseTest.Steps
 {
     [Binding]
     public class CreateAPIKeyStepDefinitions
     {
+        private static IRestClient _client;
+        private RestRequest _request;
+        private RestResponse _response;
+
         //TODO: Add logic to all step defs
 
         [Given("I create a POST request with a given username and password")]
         public void GivenICreateAPOSTRequestWithAGivenUsernameAndPassword()
         {
-            throw new PendingStepException();
+            var options = new RestClientOptions("https://videogamedb.uk");
+
+            _client = new RestClient(options);
+
+            string username = "admin";
+            string password = "admin";
+
+            _request = new RestRequest("/api/authenticate")
+                .AddJsonBody(new Dictionary<string, string> { 
+                    {"username", username }, {"password", password } 
+                });
         }
 
         [When("I send the POST request")]
         public void WhenISendThePOSTRequest()
         {
-            throw new PendingStepException();
+            _response = _client.Post(_request);
         }
 
         [Then("I receive a response with a {int} OK status code")]
         public void ThenIReceiveAResponseWithAOKStatusCode(int p0)
         {
-            throw new PendingStepException();
+            Assert.That((int)_response.StatusCode, Is.EqualTo(p0));
         }
     }
 }
