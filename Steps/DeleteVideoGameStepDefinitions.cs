b09d2bc922d7d43312d47d8c82e3c2f77759374d
gcrumpton <gcrumpton@spartaglobal.com>
2025-04-17 13:51:41 +0100

    feat: add step defs

---
+++ Steps/DeleteVideoGameStepDefinitions.cs
@@ -0,0 +1,46 @@
+using System;
+using NUnit.Framework;
+using Reqnroll;
+
+namespace VideoGameDatabaseTest.Steps
+{
+    [Binding]
+    public class DeleteVideoGameStepDefinitions
+    {
+        [Given("I create a DELETE request with valid authorisation with an ID of {int}")]
+        public void GivenICreateADELETERequestWithValidAuthorisationWithAnIDOf(int p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [Given("my DELETE request content is formatted correctly")]
+        public void GivenMyDELETERequestContentIsFormattedCorrectly()
+        {
+            throw new PendingStepException();
+        }
+
+        [When("I send the DELETE request to the specified endpoint")]
+        public void WhenISendTheDELETERequestToTheSpecifiedEndpoint()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then("The DELETE request response JSON content is formatted correctly")]
+        public void ThenTheDELETERequestResponseJSONContentIsFormattedCorrectly()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given("I create a DELETE request with invalid authorisation with an ID of {int}")]
+        public void GivenICreateADELETERequestWithInvalidAuthorisationWithAnIDOf(int p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [Given("my DELETE request content is formatted incorrectly")]
+        public void GivenMyDELETERequestContentIsFormattedIncorrectly()
+        {
+            throw new PendingStepException();
+        }
+    }
+}
